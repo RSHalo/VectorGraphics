6fbd334934aa00afe2c2b9a9d6b02c01237aadd8
Rory <rollin_ror@hotmail.com>
2020-01-07 21:12:52 +0000

    Slight zoom progress. Can zoom on a fixed position/fixed mouse position.

--- DrawingProject/Canvas.cs
+++ DrawingProject/Canvas.cs
@@ -19,6 +19,8 @@ class Canvas : Panel
     private int mouseWheelIndent;
     public float LastZoomScale { get; private set; } = 1;
     public float ZoomScale { get; private set; } = 1;
+    public PointF MouseStartZoomWorldPoint { get; private set; }
+    public PointF MouseStartZoomScreenPoint { get; private set; }
 
     public Canvas()
     {
@@ -33,6 +35,11 @@ class Canvas : Panel
     {
         base.OnMouseWheel(e);
 
+        MouseStartZoomScreenPoint = new PointF(e.X, e.Y);
+
+        MouseStartZoomWorldPoint = ScreenToWorld(e.X, e.Y);
+        Debug.WriteLine($"Mouse World: {MouseStartZoomWorldPoint.X},{MouseStartZoomWorldPoint.Y}");
+
         LastZoomScale = ZoomScale;
 
         if (e.Delta > 0)
@@ -43,5 +50,13 @@ class Canvas : Panel
         ZoomScale = mouseWheelIndent >= 0 ? mouseWheelIndent + 1 : (1 / (-mouseWheelIndent + 1f));
 
         Refresh();
+    }
+
+    public PointF ScreenToWorld(float screenX, float screenY)
+    {
+        float worldX = screenX / ZoomScale;
+        float worldY = screenY / ZoomScale;
+
+        return new PointF(worldX, worldY);
     }
 }
