c4c82e35bb1bc63562f587b2a139549a58111ae9
Rory Stuart <rory.stuart@pcf.co.uk>
2020-01-14 16:38:03 +0000

    Detecting hits for rectangles, lines and ellipses.

--- DrawingProject/Drawables/DrawableLine.cs
+++ DrawingProject/Drawables/DrawableLine.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -27,7 +28,13 @@ namespace DrawingProject.Drawables
 
         public bool HitTest(int worldX, int worldY)
         {
-            return false;
+            // A way to detect hits is to define a GraphicsPath that is the same path as the IDrawable and then call IsOutlineVisible with the mouse coordinates
+            // as parameters. This is asking "Does the mouse position touch the outline of the GraphicsPath?". In this case, the GraphicsPath is just a line.
+            var path = new GraphicsPath();
+            path.AddLine(StartPoint, EndPoint);
+
+            // Give the mouse point a pen width of 10 to make it easier to select lines. A bigger point means we don't have to be as close to the line to select it.
+            return path.IsOutlineVisible(worldX, worldY, new Pen(Color.Black, 10));
         }
     }
 }
